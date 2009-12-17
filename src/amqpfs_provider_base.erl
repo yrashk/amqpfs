44b94f30c16947f102537fac190d6af145b841bd
Yurii Rashkovskii <yrashk@scallable.com>
2009-12-17 08:15:50 -0800

    A little bit more flexible file size API for providers (now provider can override file size itself)

--- src/amqpfs_provider_base.erl
+++ src/amqpfs_provider_base.erl
@@ -1,7 +1,7 @@
--module(amqpfs_provider_base). 
+-module(amqpfs_provider_base).
 
 -export([amqp_credentials/0, init/1, list_dir/2, open/2, read/4, getattr/2,
-         object/2,
+         object/2, size/2,
          allow_request/1]).
 
 -include_lib("amqpfs/include/amqpfs.hrl").
@@ -15,7 +15,7 @@ init(State) ->
 
 list_dir(_, _State) ->
     [].
-    
+
 open(_, _State) ->
     ok.
 
@@ -34,14 +34,16 @@ read(Path, Size, Offset, State) ->
 object(_Path, _State) ->
     <<>>.
 
-getattr(Path,State) ->
-    Size = 
+size(Path, State) ->
     case amqpfs_provider:call_module(object, [Path, State], State) of
         Datum when is_list(Datum) ->
             length(Datum);
         Datum when is_binary(Datum) ->
             size(Datum)
-    end,
+    end.
+
+getattr(Path,State) ->
+    Size = amqpfs_provider:call_module(size, [Path, State], State),
     #stat{ st_size = Size }.
 
 allow_request(_State) ->
