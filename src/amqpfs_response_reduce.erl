8c79d87f22fc83e210cd03706e7c2dfb93373252
Yurii Rashkovskii <yrashk@scallable.com>
2009-12-21 15:50:12 -0800

    concat reduce function will now use max ttl instead of first ttl. makes a little bit more sense

--- src/amqpfs_response_reduce.erl
+++ src/amqpfs_response_reduce.erl
@@ -6,10 +6,13 @@
 first([H|_]) ->
     H.
 
-concat([{Thing,Ttl}|_]=L) when is_binary(Thing) ->
+concat([{Thing,_Ttl}|_]=L) when is_binary(Thing) ->
     L1 = erlang:list_to_binary(lists:map(fun({Thing1, _}) -> Thing1 end, L)),
-    {L1, Ttl};
+    {L1, max_ttl(L)};
 
-concat([{_Thing,Ttl}|_]=L) ->
+concat([{_Thing,_Ttl}|_]=L) ->
     L1 = lists:concat(lists:map(fun({Thing1, _}) -> Thing1 end, L)),
-    {L1, Ttl}.
+    {L1, max_ttl(L)}.
+
+max_ttl(Things) ->
+    lists:foldl(fun(ItemTtl, MaxTtl) -> if ItemTtl > MaxTtl -> ItemTtl; true -> MaxTtl end end, 0, lists:map(fun ({_,TtlVal}) -> TtlVal end, Things)).
