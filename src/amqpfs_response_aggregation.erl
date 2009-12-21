d7dc67cc44bd90917a924774655fc0df47efda94
Yurii Rashkovskii <yrashk@scallable.com>
2009-12-21 14:41:41 -0800

    concat aggregation

--- src/amqpfs_response_aggregation.erl
+++ src/amqpfs_response_aggregation.erl
@@ -1,7 +1,15 @@
 -module(amqpfs_response_aggregation).
--export([first/1]).
+-export([first/1, concat/1]).
 
 -include_lib("amqpfs/include/amqpfs.hrl").
 
 first([H|_]) ->
     H.
+
+concat([{Thing,Ttl}|_]=L) when is_binary(Thing) ->
+    L1 = erlang:list_to_binary(lists:map(fun({Thing1, _}) -> Thing1 end, L)),
+    {L1, Ttl};
+
+concat([{_Thing,Ttl}|_]=L) ->
+    L1 = lists:concat(lists:map(fun({Thing1, _}) -> Thing1 end, L)),
+    {L1, Ttl}.
